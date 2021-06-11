c3ce9b81e260c2c5e82d398956aadefb6493d77a
rajnish000anand <rajnish000anand@gmail.com>
2021-06-11 06:24:45 +0530

    added server info

--- index.js
+++ index.js
@@ -17,30 +17,43 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content[0]==prefix) {
-  switch(msg.content.substr(1).toLowerCase()){
+  switch(msg.content.substr(prefix.length).toLowerCase()){
       case 'ping':
         msg.channel.send('pong.');
         break;
       case 'hi':
       case 'hello':
-        msg.channel.send(`Hi @${msg.author.username}#${msg.author.discriminator}`);
+        msg.reply(`Hi`);
         break;
       case 'how are you':
       case 'how are you ?':
       case 'hru':
         msg.channel.send('All my parts seems to be 🙃 non-glitchy for now!\\🐥');
         break;
-      
       case 'all-info':
         msg.channel.send(`\`\`\`json\n${JSON.stringify(msg)}\`\`\``);
         break;
-        
+      case 'server-info':
+      case 'guild-info':
+      case 'serverinfo':
+      case 'guildinfo':
+        let dt = new Date(msg.channel.guild.joinedTimestamp);
+        msg.channel.send(`>>> **Guild info**\`\`\`md\nName : ${
+            msg.channel.guild.name
+          }\nRegion : ${
+            msg.channel.guild.region
+          }\nMembers : ${
+            msg.channel.guild.memberCount
+          }\nCreated : ${
+            dt.toString()
+          }\`\`\``);
+        break;
     };
   };
 });
 
-http.createServer(server).listen(port);
 
+http.createServer(server).listen(port);
 function server(req,res){
   res.writeHead(200,{
     'Content-Type': 'text/plain',
