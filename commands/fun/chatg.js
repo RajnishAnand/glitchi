82fef1a1c2d0a2bd602f20adb68cfce9f783b483
rajnish000anand <rajnish000anand@gmail.com>
2021-06-12 10:19:11 +0530

    add chatg

---
+++ commands/fun/chatg.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+module.exports = {
+  name: 'chatg',
+  description: 'Chat with another bot',
+  execute(message, args) {
+    if (message.channel.id !== '853117760975142922') {
+      return;
+    }
+    let url = `https://api.udit.gq/api/chatbot?message=${args.join(' ')}&nameGlitchi&user=852560696482463744&gender=female`;
+    try {
+      setTimeout(() => {
+        fetch(url)
+          .then(response => response.json())
+          .then(data => message.channel.send(`A.chata ${data.message}`));
+
+      }, 3000);
+
+    } catch (err) {
+      console.error(err);
+      message.channel.send(`${err}`);
+    }
+  },
+};
