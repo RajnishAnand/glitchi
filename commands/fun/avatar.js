aabc84c255804754f2e5031bc332276444d6c03d
rajnish000anand <rajnish000anand@gmail.com>
2021-06-13 20:09:48 +0530

    avatar command updated

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -6,37 +6,41 @@ module.exports = {
   description : 'User Avatar',
   args : false,
   execute(msg,args,client){
-    let user;
-    if(args.length){
-       if(msg.mentions.users.size){
-          user = msg.mentions.users.first();
-        }
+    let userIDs = new Array;
+    if(args.length){args.forEach(arg=>{
+        arg = arg.replace(/^<@/,'').replace(/>$/,'');
+        if(parseInt(arg)==arg)userIDs.push(arg);
         else{
-          user = msg.author;
-          try{
-            //user = client.users.cache.get(args[0]);
-            //msg.channel.send($;
-            //client.username='Testbot';
-          }
-          catch(err){
-            //msg.channel.send(err.message);
-          }
-        };
-    }
+          //userIDs.push(msg.author.id);
+          msg.channel.send(`can't resolve \`${arg}\` as a User!`);
+        }
+      });}
     else{
-      user = msg.author;
+      userIDs.push(msg.author.id);
     };
-    msg.channel.send({embed:{
+    userIDs.forEach(id=>{
+      client.users
+        .fetch(id)
+        .then(u=>sendEmbedAvatar(u,msg.channel))
+        .catch(err=>msg.channel.send(`can't resolve \`${id}\` as a User!`));
+    });
+  }
+};
+
+function sendEmbedAvatar(user,channel){
+  try{
+  channel.send({embed:{
         'color':'#00bfff',
         'title':user.username+'#'+user.discriminator,
         'image' :{
           url: `${user.displayAvatarURL({
-            format:'png',
-            dynamic:true,
+            format : 'png',
+            dynamic : 'true',
           })}?size=2048`,
         },
         'timestamp': new Date(),
         'footer':info,
       }});
   }
-};
+  catch(err){channel.send(err.message)};
+}
