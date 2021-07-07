6693c33989f939f7fd3572b289d596282e71a05b
rajnish000anand <rajnish000anand@gmail.com>
2021-07-07 11:30:34 +0530

    ready event js update

--- events/ready.js
+++ events/ready.js
@@ -3,9 +3,9 @@ module.exports = {
   name : 'ready',
   once : true,
   execute(client){
-    console.log('ready!');
-    if(process.env.PORT||false)
-      console.log('ready! port :',port);
+    const port = process.env.PORT||false;
+    console.log('ready! port :',port);
+    if(port)
       client.user.setActivity(`${prefix}commands in ${client.guilds.cache.size} servers`, { type: "LISTENING"}); 
   }
 }
