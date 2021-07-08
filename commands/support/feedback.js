cedb18a15850dc95be502d0bf4763b9b23fb1838
rajnish000anand <rajnish000anand@gmail.com>
2021-07-08 15:11:31 +0530

    updated feedback

--- commands/support/feedback.js
+++ commands/support/feedback.js
@@ -1,27 +1,23 @@
 module.exports={  
   name: 'feedback',
-  description: 'Send your feedback directly to the developer',
+  description: 'Send feedback',
   aliases: ['fd', 'suggest'],
-  usage: '[give feedback]',
-  execute(client, message, args) {
-    
+  usage: '[feedback]',
+  args : true,
+  execute({msg, args, content}){
     //support server and feedback channel respectively
-    const serverID = "856090036998635520"; 
     const channelID = "856907506612830241";
     
     //check for empty feedbacks
-    if(!args.join(" ")) return message.channel.send("Specify a feedback please!");
-    let feedback = args.join(" ");
-    
-    let embed = new Discord.MessageEmbed()    
-    .setTitle("Feedback")
-    .setColor("Green")
-    .setDescription(args.join(" "))
-    .addField('Author:', message.author)
-    .addField('From server:', message.guild.name)
-    .setTimestamp(new Date())
-    message.delete()
-    message.channel.send("Feedback sent ✅!")
-    client.guilds.get(serverID).channels.get(channelID).send(embed);
+    let embed = new Discord.msgEmbed()    
+      .setTitle("Feedback")
+      .setColor("Green")
+      .setDescription(content)
+      .addField('Author : ', msg.author)
+      .addField('From server : ', msg.guild.name)
+      .setTimestamp(new Date())
+    msg.delete();
+    msg.channel.send("Feedback sent ✅!") ;
+    msg.client.channels.get(channelID).send(embed);
   },
 }
