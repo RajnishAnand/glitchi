1e3cf07717953035febb9de043d096d8264ca07c
rajnish000anand <rajnish000anand@gmail.com>
2021-07-11 13:29:29 +0530

    ready js updated

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
   name : 'ready',
   once : true,
   execute(client){
-    const test = process.env.NODE||false ;
+    const test = process.env.SHELL||false ;
     console.log('ready! Logged in as :',client.user.tag);
     if(!test)
       client.user.setActivity(`${prefix}commands in ${client.guilds.cache.size} servers`, { type: "LISTENING"}); 
