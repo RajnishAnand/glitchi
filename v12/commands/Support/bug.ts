d42dd71768fcb08b27fd017ca9defe76a83df074
rajnish000anand <rajnish000anand@gmail.com>
2021-08-20 07:41:44 +0530

    fix :  bug command's message id

--- v12/commands/Support/bug.ts
+++ v12/commands/Support/bug.ts
@@ -20,17 +20,17 @@ class run{
     if(arg.length)embed.description='>>> '+arg;
     if(this.msg.reference)await this.msg.channel.messages.fetch(this.msg.reference.messageID).then((m:message)=>{
         embed.fields?.push({
-          name : '🌡| Referenced Message',
-          value:m.embeds.length?'```json'+`\n${JSON.stringify(m.embeds).replace(/```/g,'`­``')}}`+'```\n': `>>> ${m.content.substr(0,500)}`,
+          name : '🌡| Referenced Message :',
+          value:`­     └⊸ ID : [${this.msg.reference?.messageID}](https://discord.com/channels/${this.msg.guild.id}/${this.msg.channel.id}/${this.msg.reference?.messageID})\n`+(m.embeds.length?'```json'+`\n${JSON.stringify(m.embeds).replace(/```/g,'`­``')}}`+'```\n': `>>> ${m.content.substr(0,500)}`),
           inline:false
         })
     });
     embed.fields?.push({
       name : '🥷| Userinfo : ',
-      value : '```\n'+`Username : ${this.msg.author.tag
-          }\nID : ${this.msg.author.id
-          }\nGuild : ${this.msg.guild.name
-          }`+'```',
+      value :`├⊶ Username : \`${this.msg.author.tag
+          }\`\n├⊷ ID : \`${this.msg.author.id
+          }\`\n├⊷ Guild : \`${this.msg.guild.name
+          }\`\n└⊶ MID : [${this.msg.id}](https://discord.com/channels/${this.msg.guild.id}/${this.msg.channel.id}/${this.msg.id})`,
       inline:true,
     });
    (this.msg.client.channels.cache.get(global.config.channels.bugReport) as TextChannel).send({embed}).then((m)=>{Promise.all([
