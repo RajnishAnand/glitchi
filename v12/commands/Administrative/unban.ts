a2cbe5d27e7ab64bd9fe32b1d0546f635ee3db6c
rajnish000anand <rajnish000anand@gmail.com>
2021-08-21 20:12:18 +0530

    fix : unban command [Invalid Form Body, unknown user]

--- v12/commands/Administrative/unban.ts
+++ v12/commands/Administrative/unban.ts
@@ -7,18 +7,19 @@ class run{
     this.main(args)
   }
   main(args:string[]){
-    this.msg.react(global.config.emojis.yus);
-    this.msg.guild.members.unban(args[0])
+    const userID=args[0].replace(/^<@!?/,'').replace(/>$/,'');
+    if(!/^\d+$/.test(userID)) return this.msg.reply(global.config.emojis.evil+' Are you sure its a valid user!')
+    
+    this.msg.guild.members.unban(userID)
       .then(()=>{
-        this.msg.channel.send(`user with id :${730885117656039466}, was successfully unbanned!`);
+        this.msg.react(global.config.emojis.yus);
+        this.msg.channel.send(`user with id :${userID}, was successfully unbanned!`);
       })
       .catch((err)=>{
-        if(err.code==10026){
-          this.msg.reply('user is already unbanned!');
-        }
-        else{
-          this.msg.channel.send(err.message);
-        }
+        if(err.code==10026)
+          this.msg.reply(global.config.emojis.aha+'You are trying to ban a user which is already unbanned!');
+        else if (err.code==10013)this.msg.reply(global.config.emojis.evil+' Are you sure its a valid user!');
+        else this.msg.channel.send(err.message);
       });
   }
 }
