c1b40f89f3ade19cfd838cf78da6a2b9842bb6cf
Rajnish Anand <rajnish000anand@gmail.com>
2021-10-17 19:31:07 +0530

    add : added execute command [devOnly]

--- v13/commands/Dev/exec.ts
+++ v13/commands/Dev/exec.ts
@@ -1,21 +1,14 @@
-import { argumentObjectType, message } from '../types';
+import { argumentObjectType } from '../types';
 import cp from 'child_process';
 import util from 'util';
 import pageView from '../../libs/pagination/index';
 
-class run{
-  declare msg: message;
-  constructor ({msg,content}: argumentObjectType){
-    this.msg = msg;
-    this.main(content)
-  }
-  async main(content:string){
-    if(!this.msg.author.id===global.config.ownerID)return;
-    cp.exec (content,(...d)=>{
+function run({msg,content}:argumentObjectType){
+  if(!msg.author.id===global.config.ownerID)return;
+    cp.exec (content(),(...d)=>{
       let tx = d[0]?util.inspect(d[0]):d[1]?d[1]:d[2];
-      pageView(this.msg,{text:tx,code:'js'})
-    }) 
-  }
+      new pageView(msg,tx,{code:'BASH'})
+  }) 
 }
 
 export default {
