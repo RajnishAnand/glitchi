7ca4420f75d337b0d89fc4b2d8f053756e3302f8
Rajnish Anand <rajnish000anand@gmail.com>
2021-11-06 17:43:31 +0530

    fix : npm fix long list of maintaners

--- v13/APIs/npm.ts
+++ v13/APIs/npm.ts
@@ -6,26 +6,35 @@ export default async function npmSearch(query:string){
   
   if(!resp.total)throw Error('Any relevant search result not found!');
   
-  resp = resp.objects.map((r:any)=>{return {
-    author:{
-      name:'npm',
-      iconURL:'https://static.npmjs.com/f1786e9b7cba9753ca7b9c40e8b98f67.png'
-    },
-    color:'#fa4704',
-    title:r.package.name,
-    url:r.package.links.npm,
-    description:r.package.description,
-    fields:[{
-      name:'About :',
-      value:`‣ **Version :** ${r.package.version
-        }\n‣ **Last Publish :** <t:${Math.floor(+new Date(r.package.date)/1000)
-        }:R>${r.package.author?`\n‣ **Author :** ${r.package.author.username?`[${r.package.author.name}](https://www.npmjs.com/~${r.package.author.username})`:r.package.author.name
-        }`:''}\n‣ **Publisher :** [${r.package.publisher.username}](https://www.npmjs.com/~${r.package.publisher.username
-        })\n‣ **Maintainer(s) :** \n${r.package.maintainers.map((m:any)=>`    ⌙ [${m.username}](https://www.npmjs.com/~${m.username})`).join('\n')
-            }${r.package.keywords?`\n>>> **Keywords :** ${r.package.keywords.join(', ')}`:''}`
-    }],
-    timestamp:new Date()
-  }})
+  resp = resp.objects.map((r:any)=>{
+    const author = `${r.package.author?`\n‣ **Author :** ${r.package.author.username?`[${r.package.author.name}](https://www.npmjs.com/~${r.package.author.username})`:r.package.author.name}`:''}\n`;
+    
+    const publisher = `‣ **Publisher :** [${r.package.publisher.username}](https://www.npmjs.com/~${r.package.publisher.username})\n`;
+    
+    const large= r.package.maintainers.length>15&&
+      (r.package.maintainers.length=15);
+    const maintainers = `‣ **Maintainer(s) :** \n${r.package.maintainers.map((m:any)=>`    ⌙ [${m.username}](https://www.npmjs.com/~${m.username})`).join('\n')}${large?'...':''}`;
+    
+    const keywords = `${r.package.keywords?`\n>>> **Keywords :** ${r.package.keywords.join(', ')}`:''}`;
+    
+    return {
+      author:{
+        name:'npm',
+        iconURL:'https://static.npmjs.com/f1786e9b7cba9753ca7b9c40e8b98f67.png'
+      },
+      color:'#fa4704',
+      title:r.package.name,
+      url:r.package.links.npm,
+      description:r.package.description,
+      fields:[{
+        name:'About :',
+        value:`‣ **Version :** ${r.package.version
+          }\n‣ **Last Publish :** <t:${Math.floor(+new Date(r.package.date)/1000)
+          }:R>${author}${publisher}${maintainers}${keywords}`
+      }],
+      timestamp:new Date()
+    }
+  })
   return resp;
 }
 
