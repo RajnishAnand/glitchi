7f81fde5b9058790ef47de6e4157483dc47027c2
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-02-12 23:49:15 +0530

    add: get quotes by id

---
+++ v13/commands/Fun/quote.ts
@@ -0,0 +1,26 @@
+import {Command} from 'Interfaces';
+import {quoteById,randomQuote} from '../../APIs/@quotes';
+
+export const command: Command ={
+  name: "quote",
+  aliases: ["quotable"],
+  description: "Get quotes from qoutable API",
+  async run({msg, args}){
+    // (default) random quote 
+    if(!args.length||args[0].toLowerCase()=="random") 
+      return msg.reply({
+        embeds : [await randomQuote()],
+        allowedMentions:{
+          repliedUser:false
+        }
+      });
+
+    // Quote with a specific id
+    msg.reply({
+      embeds: [await quoteById(args[0])],
+      allowedMentions: {
+        repliedUser: false
+      }
+    })
+  }
+}
