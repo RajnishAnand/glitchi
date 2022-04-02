ce4380edcdf1af1dc13bcff9a5e1e46b9e00e8b9
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-04-02 14:22:24 +0530

    add: add incomplete mathsteps types

---
+++ v13/typings/mathsteps.d.ts
@@ -0,0 +1,33 @@
+declare module 'mathsteps' {
+  export function simplifyExpression(equation:string) {
+    return Steps;
+  }
+}
+
+type Steps = Status[];
+
+interface Status {
+  changeType : string;
+  oldEquation: Equation;
+  newEquation: Equation;
+  stubsteps: Status[]|[];
+}
+
+interface Equation {
+  leftNode: Node;
+  rightNode: Node;
+  comparator: string;
+}
+
+interface Node {
+  implicit: boolean;
+  op: string;
+  fn: string;
+  args:[] // CHECK: 
+
+  //TODO: differenciate left right nodes
+  value: string;
+  valueType: string;
+
+  changeGroup: number| undefined;
+}
