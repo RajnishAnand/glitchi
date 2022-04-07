1473a17761e66c7c492d2f36552f758a70944156
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-04-07 10:19:29 +0530

    add: Added OwOify slash command

---
+++ v13/commands-slash/owoify.ts
@@ -0,0 +1,20 @@
+import { owoify } from "#libs";
+import { SlashCommand } from "Interfaces";
+
+export const command: SlashCommand = {
+  name: "owoify",
+  description: "owoify your text",
+  options: [{
+    name: "text",
+    description: "Text to owoify",
+    type: "STRING",
+    required: true
+  }],
+
+  run({interaction}){
+    const txt=interaction.options.getString("text") as string;
+    interaction.reply({
+      content: owoify(txt.toLowerCase())
+    })
+  }
+}
