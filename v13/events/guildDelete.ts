92ccccbb61a07f2f7f84f53d6929fd7a57c0e640
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-04-13 19:55:39 +0530

    add: added swrver lost log

---
+++ v13/events/guildDelete.ts
@@ -0,0 +1,36 @@
+import {Guild, TextChannel} from "discord.js";
+import {Event} from "../Interfaces";
+
+export const event : Event = {
+  name : 'guildDelete',
+
+  execute(client,guild: Guild){
+    // set status when in production
+    if(!process.env.BETA)client.user!.setPresence({
+      status:'online',
+      activities:[{
+        name : `${client.config.prefix} commands in ${client.guilds.cache.size} servers`,
+        type: "LISTENING"
+      }]
+    });
+
+
+    const channel = client.channels.cache.get(client.config.channels.serverLog);
+    if(!channel|| !(channel instanceof TextChannel))return;
+
+    channel.send({
+      embeds : [{
+        title : `Server Lost ${client.config.emojis.cry}`,
+        description : `\`\`\`js\n{\n  name: ${guild.name}\n  ID: ${guild.id}\n  guildDescription: ${guild.description}\n  ownerId: ${guild.ownerId} \n}\`\`\``,
+        thumbnail : {
+          url : guild.iconURL({
+            dynamic:true,
+            size : 4096
+          })??"https://discord.com/assets/ca03beabe94d8f97ba6fbf75cbb695c4.png",
+        },
+        timestamp : new Date(),
+        color:'#ff5500'
+      }]
+    })
+  }
+}
