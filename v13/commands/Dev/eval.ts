824882232d2308749f0db45083933a9c30ec5739
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-04-14 14:36:02 +0530

    nn: nochange

--- v13/commands/Dev/eval.ts
+++ v13/commands/Dev/eval.ts
@@ -1,5 +1,5 @@
 import util from 'util';
-import {pageView,firebase} from '#libs';
+import {pageView} from '#libs';
 import {Command} from 'Interfaces';
 
 export const command: Command = {
@@ -15,7 +15,7 @@ export const command: Command = {
     try {
       if (msg.author.id === msg.client.guilds.cache.get(msg.client.config.guildId)?.ownerId) {
         const client = msg.client;
-        const db=()=>{firebase.once("value",(d)=>send(d.val()))};
+        // const db=()=>{firebase.once("value",(d)=>send(d.val()))};
         const send=(text:string, bool:boolean=false)=>{
           if(bool)new pageView(msg,text);
           else new pageView(msg,debug(text),
