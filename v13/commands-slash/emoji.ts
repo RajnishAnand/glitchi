99028db0caa5e574e8615c7855d69ca5c9b547a4
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-04-25 17:53:36 +0530

    add: emoji slash command size option

--- v13/commands-slash/emoji.ts
+++ v13/commands-slash/emoji.ts
@@ -2,19 +2,33 @@ import {SlashCommand} from 'Interfaces';
 import {Collection, GuildEmoji} from 'discord.js';
 
 export const command : SlashCommand= {
-  name : 'e',
+  name : 'emo',
   description : 'sends emoji for you!',
-  options : [{
-    name : 'query',
-    description : 'emoji name to query',
-    type : 'STRING',
-    required : true,
-    autocomplete: true,
-  }],
+  options : [
+    {
+      name : 'query',
+      description : 'emoji name to query',
+      type : 'STRING',
+      required : true,
+      autocomplete: true,
+    },
+    {
+      name: "size",
+      description: "raw emoji size:",
+      type: 'STRING',
+      required: false,
+      choices: ["128", "96", "80", "64", "48", "32", "16"]
+        .map(e=>({name:`${e}x${e}`,value:e}))
+    }
+  ],
     
   run({interaction}){
     const q=(interaction.options.getString('query') as string);
-    let emoji: string|undefined = findEmojis(interaction.client.emojis.cache,q.split("#")[0])[q];
+    const size=interaction.options.getString('size');
+
+    let emojiData= findEmojis(interaction.client.emojis.cache,q.split("#")[0])[q];
+    
+    const emoji: string = size?`https://cdn.discordapp.com/emojis/${emojiData.id}.${emojiData.animated?"gif":"png"}?size=${size}`:`<${emojiData.animated?"a":""}:${emojiData.name}:${emojiData.id}>`;
 
     if(emoji)interaction.reply(`${emoji}`);
     else interaction.reply({
@@ -34,7 +48,13 @@ export const command : SlashCommand= {
 }
 
 
-type SearchedEmoji = {[key:string]:string};
+type SearchedEmoji = {
+  [key:string]:{
+    name:string,
+    id:string,
+    animated:boolean
+  }
+};
 
 // Emoji search Function
 function findEmojis(emoji:Collection<string,GuildEmoji>,q:string):SearchedEmoji{
@@ -44,15 +64,23 @@ function findEmojis(emoji:Collection<string,GuildEmoji>,q:string):SearchedEmoji{
 
   const result:SearchedEmoji = {};
   for(let [_,e] of emojis){
-
     if(Object.keys(result).length>24)return result;
-    if(!(`${e.name}` in result)){
-      result[`${e.name}`] = e.url;
+    if(!e.name)continue;
+    if(!(e.name in result)){
+      result[`${e.name}`] = {
+        name: e.name,
+        id: e.id,
+        animated: e.animated||false
+      };
       continue;
     }
     for(let i=1;i<25;i++){
       if(!(`${e.name}#${i}` in result)){
-        result[`${e.name}#${i}`] = e.url;
+        result[`${e.name}#${i}`] = {
+          name: e.name,
+          id: e.id,
+          animated: e.animated||false
+        };
         break;
       }
     };
