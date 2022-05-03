517abd11a4c916f3fdebb3bf0caaf4b10659df22
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-05-04 02:11:10 +0530

    update: update quote api

--- v13/commands/Fun/quote.ts
+++ v13/commands/Fun/quote.ts
@@ -1,3 +1,4 @@
+import { MessageEmbed } from 'discord.js';
 import {Command} from 'Interfaces';
 import {quoteById,randomQuote} from '../../APIs/@quotes';
 
@@ -6,18 +7,15 @@ export const command: Command ={
   aliases: ["quotable"],
   description: "Get quotes from qoutable API",
   async run({msg, args}){
-    // (default) random quote 
-    if(!args.length||args[0].toLowerCase()=="random") 
-      return msg.reply({
-        embeds : [(await randomQuote()).embedify()],
-        allowedMentions:{
-          repliedUser:false
-        }
-      });
-
+    let quote:MessageEmbed|undefined;
+    try{
+      if(!args.length||args[0].toLowerCase()=="random") 
+        quote = await randomQuote().then(q=>q.embedify());
+      else quote = await quoteById(args[0]).then(r=>r.embedify())
+    } catch (err:any){msg.reply(err.message)}
     // Quote with a specific id
-    msg.reply({
-      embeds: [await quoteById(args[0])],
+    if(quote)msg.reply({
+      embeds: [quote],
       allowedMentions: {
         repliedUser: false
       }
