0f3785640101409f3c624dde1f3da71c7558af52
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-05-04 02:04:04 +0530

    update: updated quote api

--- v13/APIs/@quotes/index.ts
+++ v13/APIs/@quotes/index.ts
@@ -1,12 +1,13 @@
 import fetch from "node-fetch";
 import {MessageEmbed} from 'discord.js';
-import {QuoteRoutes, RandomFilterOptions, QuoteResponse, ErrorResponse} from "./types";
+import {RandomFilterOptions, QuoteResponse, ErrorResponse} from "./types";
 
 /** API URL */
 const URL='https://api.quotable.io';
 
+
 /** Returns (a Quote or Error) as MessageEmbed */
-async function randomQuote(filters?:RandomFilterOptions){
+export async function randomQuote(filters?:RandomFilterOptions){
   let options :string|undefined;
   if(filters)options= `?${
     filters.author?`author=${encodeURI(filters.author)}&`:''
@@ -18,40 +19,29 @@ async function randomQuote(filters?:RandomFilterOptions){
     filters.tags?`tags=${encodeURI(filters.tags)}`:''
   }`;
 
-  const resp: QuoteResponse|ErrorResponse= await fetchit("random",options);
+  const value: QuoteResponse|ErrorResponse= await fetch(`${URL}/random/${options}`).then(r=>r.json());
+  if(!("_id" in value)) throw new Error(value.statusMessage);
   return {
-    value : resp,
+    value,
     embedify(){return decorate(this.value)}
   };
 }
 
+
 /** Returns a Quote with id if found else error message*/
-async function quoteById(id:string):Promise<MessageEmbed>{
-  return decorate((await fetchit("quotes",'/'+id) as QuoteResponse));
+export async function quoteById(id:string){
+  const value:QuoteResponse|ErrorResponse = await fetch(`${URL}/quotes/${id}`).then(r=>r.json());
+  if(!("_id" in value)) throw new Error(value.statusMessage);
+  
+  return {
+    value,
+    embedify(){return decorate(this.value)}
+  }
 }
 
-
-// TODO: add more field and Options
-
-
-/////////////// Helper functions ////////////
-
-async function fetchit(route:QuoteRoutes,options:string=''){
-  return await fetch(`${URL}/${route}${options}`)
-    .then(r=>r.json())
-}
 
 /** decorates a QuoteResponse or ErrorResponse into an embed*/
-function decorate(d:QuoteResponse|ErrorResponse){
-  if(!("_id" in d)) return new MessageEmbed({
-    color: "#ff5500",
-    author: {
-      name : "Quotable: ",
-      url : URL
-    },
-    description:`\`\`\`js\n${JSON.stringify(d,null,'  ')} \`\`\``
-  });
-
+function decorate(d:QuoteResponse):MessageEmbed{
   return new MessageEmbed({
     color: "#fcc300",
     author: {
@@ -71,5 +61,3 @@ function decorate(d:QuoteResponse|ErrorResponse){
   })
 }
 
-///// exportings ////
-export {randomQuote,quoteById};
