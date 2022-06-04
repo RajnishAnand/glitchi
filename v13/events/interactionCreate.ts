e42a038bc691dd393bdd63a3a308e6f74d172eda
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-06-04 17:57:32 +0530

    update:interactionCreate event handler for 1 2 3 type

--- v13/events/interactionCreate.ts
+++ v13/events/interactionCreate.ts
@@ -3,18 +3,28 @@ import {Event, ExtendInteraction} from '../Interfaces';
 export const event : Event =  {
   name : 'interactionCreate',
   execute(client,interaction:ExtendInteraction){
-    if (!(interaction.isAutocomplete()||interaction.isCommand()))return;
+    if (!(
+      interaction.isAutocomplete()||
+      interaction.isCommand()||
+      interaction.isContextMenu()
+    ))return;
 
-    const cmnd = client.slashCommands.get(interaction.commandName);
+    const cmnd= client.slashCommands.get(interaction.commandName);
     if(!(cmnd && interaction.guildId)){
       return;
     };
+    
+    // AutocompleteInteraction
+    if(interaction.isAutocomplete()&&cmnd.autocompleteRun&&(!interaction.responded))cmnd.autocompleteRun({client,interaction});
 
-    if(interaction.isAutocomplete()){
-      if(!cmnd.autocompleteRun || interaction.responded)return;
-      cmnd.autocompleteRun({client,interaction});
-    }
-    else if(!interaction.replied) 
-      cmnd.run({client, interaction});
+    // CommandInteraction 
+    else if(interaction.isCommand()&&(!cmnd.type || cmnd.type=="CHAT_INPUT")&&interaction.isRepliable())cmnd.run({client,interaction});
+
+    // UserContextMenuInteraction
+    else if(interaction.isUserContextMenu()&&(cmnd.type=="USER"))cmnd.run({client,interaction});
+
+    // MessageContextMenuInteraction
+    else if(interaction.isMessageContextMenu()&&(cmnd.type=="MESSAGE"))cmnd.run({client,interaction});
   }
+
 }
