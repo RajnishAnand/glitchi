3cfcefacca97868baa44b05dbad39cf1e8e85063
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-06-10 17:22:57 +0530

    🎸add: Pacman api types (incomplete)

---
+++ v13/APIs/@pac/types.d.ts
@@ -0,0 +1,35 @@
+export type PacResponse = {
+  version: 2
+  limit: 250
+  valid: true
+  results: {
+    pkgname: string;
+    pkgbase: string;
+    repo: string;
+    arch: string;
+    pkgver: string;
+    pkgrel: string;
+    epoch: numbee;
+    pkgdesc: string;
+    url: URL;
+    filename: string;
+    compressed_size: number;
+    installed_size: number;
+    build_date: string;
+    last_update: string;
+    flag_date: null|string;
+    maintainers: string[];
+    packager: string;
+    groups: string[]|[];
+    licenses: string[];
+    conflicts: string[]|[];
+    provides: string[]|[];
+    replaces: string[]|[];
+    depends: string[];
+    optdepends: string[]|[];
+    makedepends: string[]|[];
+    checkdepends: string[]|[];
+  }[]|[];
+  num_pages: number;
+  page: number;
+}
