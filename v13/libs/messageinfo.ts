051125919a270dc0d264edd3bc6b175adf4a1c12
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-06-19 07:15:43 +0530

    👻add: messageinfo as a lib

---
+++ v13/libs/messageinfo.ts
@@ -0,0 +1,56 @@
+import { Message } from "discord.js";
+import { inspect } from "util";
+import { ObjectPaginationData } from "./pageview";
+
+export default function mInfo(m0:Message){
+  let m:Partial<Message> = Object.assign({},m0);
+  const data:ObjectPaginationData = [];
+
+  // content
+  const content = m.content!.length
+    ?inspect(m.content):undefined;
+  delete m.content;
+
+  // embed 
+  const embeds = m.embeds!.length
+    ?JSON.stringify(m.embeds,null,"  "):undefined;
+  delete m.embeds;
+  
+  // author 
+  const author = inspect(m.author);
+  delete m.author;
+  
+  data.push({
+    title: "Overview",
+    description: "Message Overview",
+    text : inspect(m),
+    emoji: "🤘",
+    options: {split:{with:","},decoration:{lang:"js",title:"Messageinfo[Overview]"}}
+  }) 
+
+  data.push({
+    title: "Author",
+    description: "Message's Author Info",
+    text : author,
+    emoji: "🕵️",
+    options: {split:{with:","},decoration:{lang:"js",title:"Messageinfo[Author]"}}
+  })
+
+  if(embeds)data.push({
+    title: "Embeds",
+    description: "Message Embeds in JSON",
+    text : embeds,
+    emoji: "🏺",
+    options: {split:{with:","},decoration:{lang:"js",title:"Messageinfo[Embeds]"}}
+  });
+
+  if(content)data.push({
+    title: "Content",
+    description: "Message Content",
+    text: content,
+    emoji: "🐏",
+    options: {decoration:{title:"Messageinfo[Content]"}}
+  });
+
+  return data;
+}
