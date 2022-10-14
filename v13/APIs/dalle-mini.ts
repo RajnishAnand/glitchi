cdf6adab0bf92be8f669f986bd17e3af4c90df94
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-10-14 10:23:04 +0530

    🎸add: added dallemini api

---
+++ v13/APIs/dalle-mini.ts
@@ -0,0 +1,11 @@
+export default async function dallemini(prompt:string) {
+  return await fetch ("https://bf.dallemini.ai/generate",{
+    method: "POST",
+    headers: { 'Content-Type': "application/json" },
+    body: JSON.stringify({prompt})
+  })
+    .then(r=>r.json())
+    .then((r:{images:string[]})=>{
+      return r.images.map(i=>Buffer.from(i, "base64"))
+    });
+}
