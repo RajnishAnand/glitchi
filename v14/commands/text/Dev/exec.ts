15c6aa24e9bba86f381829ecea915d48b3e354d2
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-10-24 17:27:29 +0530

    👻update: updated exec to v14

--- v14/commands/text/Dev/exec.ts
+++ v14/commands/text/Dev/exec.ts
@@ -1,20 +1,18 @@
 import cp from 'child_process';
 import util from 'util';
 import { stringPagination } from '#libs';
-import { Command } from 'Interfaces';
+import { TextCommand } from 'client/interface';
 
-export const command: Command = {
+export const command: TextCommand = {
   name: 'exec',
   description: 'execute command directly to bash',
-  aliases: ['ex'],
-  // usage : string,
+  aliases: ['ex', '$'],
   args: true,
-  // permissions : string,
-  devOnly: true,
-  // permRequired : [string],
-  // examples : [string],
-  run({ msg, content }) {
-    if (!(msg.author.id === msg.client.config.ownerId)) return;
+  argsHelp: ['...<code>'],
+  ownerOnly: true,
+
+  run({ client, msg, content }) {
+    if (!(msg.author.id === client.config.ownerId)) return;
     cp.exec(content(), (...d) => {
       let tx = d[0] ? util.inspect(d[0]) : d[1] ? d[1] : d[2];
       new stringPagination(msg, tx, {
