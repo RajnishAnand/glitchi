dc8be30a95ce180dbb977abd3ca383970b1479db
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-11-13 11:09:32 +0530

    👻update: updated ping to v14

--- v14/commands/text/Fun/ping.ts
+++ v14/commands/text/Fun/ping.ts
@@ -1,16 +1,16 @@
+import { TextCommand } from 'client/interface';
 import { Message } from 'discord.js';
-import { Command } from 'Interfaces';
 
-export const command: Command = {
+export const command: TextCommand = {
   name: 'ping',
   description: 'bot latency and heartbeat',
   args: false,
-  run({ msg }) {
+  run({ client, msg }) {
     msg.channel
       .send(`pong!`)
       .then((sent: Message) =>
         sent.edit(
-          `${msg.client.config.emojis.dance} | Pong! | Heartbeat : ${
+          `${client.config.emojis.dance} | Pong! | Heartbeat : ${
             msg.client.ws.ping
           }ms | Roundtrip latency : ${
             sent.createdTimestamp - msg.createdTimestamp
