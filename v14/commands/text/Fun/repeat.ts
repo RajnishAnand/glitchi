524db1654af2314a85e3484741a625a79b5a266d
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2022-11-13 11:47:06 +0530

    👻update: updated repeat command to v14

--- v14/commands/text/Fun/repeat.ts
+++ v14/commands/text/Fun/repeat.ts
@@ -1,20 +1,20 @@
-import { Command } from 'Interfaces';
+import { TextCommand } from 'client/interface';
 
-export const command: Command = {
+export const command: TextCommand = {
   name: 'repeat',
   description: 'make glitchi repeat your text',
   aliases: ['say'],
   args: true,
   examples: ['hello', 'hi'],
-  run({ msg, commandName }) {
+  run({ client, msg, commandName }) {
     let txt = msg.cleanContent
-      .substring(msg.client.config.prefix.length)
+      .substring(client.config.prefix.length)
       .replace(/^[\s+]?/, '')
       .replace(commandName + ' ', '');
 
     if (/@((here)|(everyone))/.test(txt))
       msg.reply(
-        `${msg.client.config.emojis.knife} Are you trying to make me ping everyone.`,
+        `${client.config.emojis.knife} Are you trying to make me ping everyone.`,
       );
     else msg.channel.send(txt);
   },
